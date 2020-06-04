06fb3a68a4a5cfdaa2da53cc0708331a1f687d9c
mithunansivakumar <mithunan2522@gmail.com>
2020-06-04 16:35:08 +0200

    MainScreen Update

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -53,20 +54,78 @@ class _MyHomePageState extends State<MyHomePage> {
           ),
         ),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+      body: Column(
+        children: <Widget>[
+          SizedBox(
+            height: 50.0,
+          ),
+          Text(
+            "Simple way to find",
+            style: TextStyle(
+              color: Colors.black,
+              fontWeight: FontWeight.bold,
+              fontSize: 25.0,
             ),
-            Text(
-              '$_counter',
+          ),
+          Text(
+            "Tasty recipes",
+            style: TextStyle(
+              color: Colors.deepOrangeAccent,
+              fontWeight: FontWeight.bold,
+              fontSize: 25.0,
             ),
-          ],
-        ),
+          ),
+          SizedBox(
+            height: 25.0,
+          ),
+          Row(
+            children: <Widget>[
+              Container(
+                  height: 350.0,
+                  width: 250.0,
+                  color: Colors.black,
+                  margin: EdgeInsets.symmetric(
+                    vertical: 16.0,
+                    horizontal: 24.0,
+                  ),
+                  child: Stack(
+                    children: <Widget>[
+                      recipeCard,
+                      recipeThumbnail,
+                    ],
+                  )),
+              SizedBox(
+                width: 10.0,
+              ),
+              Container(
+                child: Text("Card"),
+                height: 350.0,
+                width: 10.0,
+                color: Colors.black,
+              ),
+            ],
+          ),
+          SizedBox(
+            height: 25.0,
+          ),
+          Container(
+            color: Colors.deepOrangeAccent,
+            height: 30.0,
+            width: 200.0,
+            child: Text(
+              "Add new recipe",
+              style: TextStyle(
+                fontSize: 20.0,
+                color: Colors.white,
+              ),
+              textAlign: TextAlign.center,
+            ),
+          ),
+        ],
       ),
+
       floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.deepOrangeAccent,
         onPressed: _incrementCounter,
         tooltip: 'Increment',
         child: Icon(Icons.add),
@@ -74,3 +133,29 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 }
+
+/*Widgets */
+
+final recipeCard = new Container(
+  height: 350.0,
+  margin: EdgeInsets.only(
+    left: 10.0,
+  ),
+  decoration: BoxDecoration(
+    color: Colors.deepOrangeAccent,
+    shape: BoxShape.rectangle,
+    borderRadius: BorderRadius.circular(8.0),
+  ),
+);
+
+final recipeThumbnail = new Container(
+  margin: EdgeInsets.symmetric(
+    vertical: 16.0,
+  ),
+  alignment: FractionalOffset.centerLeft,
+  child: Image(
+    image: AssetImage("Images/recipe1.jpg"),
+    height: 92.0,
+    width: 92.0,
+  ),
+);
